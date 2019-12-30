ac9decf854f580179f9e763efa064360d4556c44
swetha-zastra <swetha@zastraitsolutions.com>
2019-12-30 13:21:54 +0530

    Compliance Calendar Application

---
+++ app/src/main/java/Zastra/B5Calendar/PagerAdapter.java
@@ -0,0 +1,42 @@
+package Zastra.B5Calendar;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import Zastra.B5Calendar.Calendar.CalendarFragment;
+import Zastra.B5Calendar.EventsHIstory.EventHistoryFragment;
+
+class PagerAdapter extends FragmentPagerAdapter {
+
+    int numberOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.numberOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                CalendarFragment calendarFragment = new CalendarFragment();
+                return calendarFragment;
+
+            case 1:
+
+                EventHistoryFragment eventHistoryFragment = new EventHistoryFragment();
+                return eventHistoryFragment;
+
+            default:
+
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+
+        return numberOfTabs;
+    }
+}
