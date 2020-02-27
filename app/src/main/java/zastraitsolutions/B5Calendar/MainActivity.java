d4ecbd458b594ff1923e20b5af38802feae31f18
swetha-zastra <swetha@zastraitsolutions.com>
2020-02-27 10:04:07 +0530

    B5

---
+++ app/src/main/java/zastraitsolutions/B5Calendar/MainActivity.java
@@ -0,0 +1,14 @@
+package zastraitsolutions.B5Calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
